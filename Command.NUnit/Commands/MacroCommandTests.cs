23557d305ce7113b9b2d674122ee4f79829390b4
garabavlad <garaba.vlad@gmail.com>
2025-11-09 11:34:14 +0100

    Fix unit tests

--- Command.NUnit/Commands/MacroCommandTests.cs
+++ Command.NUnit/Commands/MacroCommandTests.cs
@@ -21,47 +21,26 @@ public class MacroCommandTests
     }
 
     [Test]
-    public void Execute_DemonstratesBrokenChaining()
-    {
-        // Arrange - This test demonstrates the bug
-        var document = new Document();
-
-        // All commands created with the same initial empty document
-        var commands = new List<Func<Document, ICommand<Document>>>
-        {
-            doc => new InsertCommand(doc, 0, "A"), // Should insert at 0
-            doc => new InsertCommand(doc, 1, "B"), // Expects position 1 exists
-            doc => new InsertCommand(doc, 2, "C"), // Expects position 2 exists
-        };
-
-        var macroCommand = new MacroCommand(document, commands);
-
-        // Act & Assert
-        // This will throw ArgumentOutOfRangeException because all commands
-        // operate on the same empty document, not on the result of previous commands
-        Assert.Throws<ArgumentOutOfRangeException>(() => macroCommand.Execute());
-    }
-
-    [Test]
     public void Execute_WithSameDocumentState_ThrowsOnInvalidPositions()
     {
         // Arrange
         var document = new Document();
         document = document.InsertText(0, "Start");
 
-        // Commands that would work if chained, but won't work because
-        // they all reference the same "Start" document state
+        // Commands that would work if chained
         var commands = new List<Func<Document, ICommand<Document>>>
         {
-            doc => new InsertCommand(doc, 5, " A"), // OK: "Start A"
-            doc => new InsertCommand(doc, 7, "B"), // FAIL: position 7 doesn't exist in "Start"
-            doc => new InsertCommand(doc, 8, "C"), // FAIL: position 8 doesn't exist in "Start"
+            doc => new InsertCommand(doc, 5, " A"),
+            doc => new InsertCommand(doc, 7, "B"),
+            doc => new InsertCommand(doc, 8, "C"),
         };
 
         var macroCommand = new MacroCommand(document, commands);
 
         // Act & Assert
-        Assert.Throws<ArgumentOutOfRangeException>(() => macroCommand.Execute());
+        var result = macroCommand.Execute();
+
+        Assert.That(result.Content, Is.EqualTo("Start ABC"));
     }
 
     [Test]
@@ -100,6 +79,7 @@ public class MacroCommandTests
         var macroCommand = new MacroCommand(document, commands);
 
         // Act
+        macroCommand.Execute();
         var description = macroCommand.Description;
 
         // Assert
